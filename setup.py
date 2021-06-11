793b8e7e63930603b817b69f8f2302f3a4fe7723
Daochen Zha <daochen.zha@tamu.edu>
2021-06-10 21:59:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     author="Kuai",
     author_email="daochen.zha@tamu.edu",
     description="DouZero DouDizhu AI",
-    long_description='long_description',
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
     license='Apache License 2.0',

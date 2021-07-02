a3a09e20e9c24e4b4af79bdfd8061d2c294db1d3
Daochen Zha <daochen.zha@tamu.edu>
2021-07-02 00:51:53 -0500

    Update eval

--- douzero/evaluation/simulation.py
+++ douzero/evaluation/simulation.py
@@ -3,11 +3,6 @@ import pickle
 
 from douzero.env.game import GameEnv
 
-num_landlord_wins = mp.Value('i', 0)
-num_farmer_wins = mp.Value('i', 0)
-num_landlord_scores = mp.Value('i', 0)
-num_farmer_scores = mp.Value('i', 0)
-
 def load_card_play_models(card_play_model_path_dict):
     players = {}
 
@@ -23,7 +18,7 @@ def load_card_play_models(card_play_model_path_dict):
             players[position] = DeepAgent(position, card_play_model_path_dict[position])
     return players
 
-def mp_simulate(card_play_data_list, card_play_model_path_dict):
+def mp_simulate(card_play_data_list, card_play_model_path_dict, q):
 
     players = load_card_play_models(card_play_model_path_dict)
 
@@ -34,17 +29,11 @@ def mp_simulate(card_play_data_list, card_play_model_path_dict):
             env.step()
         env.reset()
 
-    with num_landlord_wins.get_lock():
-        num_landlord_wins.value += env.num_wins['landlord']
-
-    with num_farmer_wins.get_lock():
-        num_farmer_wins.value += env.num_wins['farmer']
-
-    with num_landlord_scores.get_lock():
-        num_landlord_scores.value += env.num_scores['landlord']
-
-    with num_farmer_scores.get_lock():
-        num_farmer_scores.value += env.num_scores['farmer']
+    q.put((env.num_wins['landlord'],
+           env.num_wins['farmer'],
+           env.num_scores['landlord'],
+           env.num_scores['farmer']
+         ))
 
 def data_allocation_per_worker(card_play_data_list, num_workers):
     card_play_data_list_each_worker = [[] for k in range(num_workers)]
@@ -67,15 +56,33 @@ def evaluate(landlord, landlord_up, landlord_down, eval_data, num_workers):
         'landlord_up': landlord_up,
         'landlord_down': landlord_down}
 
-    pool = mp.Pool(processes=num_workers)
-    results = [pool.apply_async(mp_simulate,
-                                args=(card_play_data,
-                                      card_play_model_path_dict))
-               for card_play_data in card_play_data_list_each_worker]
+    num_landlord_wins = 0
+    num_farmer_wins = 0
+    num_landlord_scores = 0
+    num_farmer_scores = 0
 
-    results = [p.get() for p in results]
-    num_total_wins = num_landlord_wins.value + num_farmer_wins.value
+    ctx = mp.get_context('spawn')
+    q = ctx.SimpleQueue()
+    processes = []
+    for card_paly_data in card_play_data_list_each_worker:
+        p = ctx.Process(
+                target=mp_simulate,
+                args=(card_paly_data, card_play_model_path_dict, q))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    for i in range(num_workers):
+        result = q.get()
+        num_landlord_wins += result[0]
+        num_farmer_wins += result[1]
+        num_landlord_scores += result[2]
+        num_farmer_scores += result[3]
+
+    num_total_wins = num_landlord_wins + num_farmer_wins
     print('WP results:')
-    print('landlord : Farmers - {} : {}'.format(num_landlord_wins.value / num_total_wins, num_farmer_wins.value / num_total_wins))
+    print('landlord : Farmers - {} : {}'.format(num_landlord_wins / num_total_wins, num_farmer_wins / num_total_wins))
     print('ADP results:')
-    print('landlord : Farmers - {} : {}'.format(num_landlord_scores.value / num_total_wins, 2 * num_farmer_scores.value / num_total_wins)) 
+    print('landlord : Farmers - {} : {}'.format(num_landlord_scores / num_total_wins, 2 * num_farmer_scores / num_total_wins)) 
